df66578effd06f3d69a7ec918f9853bbcd3f3c11
Liam Reeves <wesleyliam005@gmail.com>
2024-11-07 19:08:47 +0300

    first commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+
+    printf("Hello, Liam , Welcome to C language \n");
+    printf("C is the mother of languages\n");
+
+
+    return 0; //exit status of a program 
+}
+
+// Comments 
+// single line comment
+
+/*Multi line comment */
+
---
+++ variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h> // To include Booleans i.e  True or False
+
+
+int main(void){
+        // variables in C include: Integer, float, Char, Array, Booleans 
+
+     int age = 21;
+     float GPA = 2.05;
+     char name[] = "Bro"; // to display a character array we use %s
+     bool Ans = true;
+     char grade = 'C';
+
+
+
+     printf("Hello %s\n", name );
+     printf("You are %d years old\n",age);
+     printf("Your mean is %.2f\n",GPA);
+     // to display a double float use %lf 
+    printf("You're are client %d\n ", Ans);
+    printf("Average is %c\n", grade);
+
+   return 0;
+}
